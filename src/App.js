04f105afffe277eac4ab1a78f1a0a476d521231b
Stanislav Mozolevskiy <42652969+Stanmozolevskiy@users.noreply.github.com>
2023-04-30 10:12:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Production PROD
+         Production Dev
         </a>
       </header>
     </div>
