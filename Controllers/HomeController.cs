7ba0bdbe5d25d3f2637c4ebae89b44ad37f8dcac
stanmozolevskiy <stanmozolevskiy@gmail.com>
2023-05-12 07:57:31 -0400

    .net project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace ielts_backend.Controllers
+{
+    [Route("[controller]"), ApiController, AllowAnonymous]
+    public class HomeController : ControllerBase 
+    { 
+        [HttpGet]
+        public IActionResult Echo(string message) => Ok("test");
+
+
+    }
+}
